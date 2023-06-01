0a6c175fb89f30972f13214c68060ea2511ee8d4
Jordy <jordycorporan95@gmail.com>
2023-06-01 11:25:53 -0400

    Modifying a migration

--- Migration_Demo_2/BlueBird/db/migrate/20230601142136_create_users.rb
+++ Migration_Demo_2/BlueBird/db/migrate/20230601142136_create_users.rb
@@ -7,8 +7,8 @@ class CreateUsers < ActiveRecord::Migration[7.0]
       t.timestamps
     end
 
-    add_index :users, :username, unique: true
-    add_index :users, :email, unique: true
-    
+    # add_index :users, :username, unique: true
+    # add_index :users, :email, unique: true
+
   end
 end
---
+++ Migration_Demo_2/BlueBird/db/migrate/20230601150452_banana.rb
@@ -0,0 +1,4 @@
+class Banana < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
