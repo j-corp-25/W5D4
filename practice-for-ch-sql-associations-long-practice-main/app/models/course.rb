efd2f01dc97741fdc5a7a4775dbe3df044a549eb
JasonStaubach <jbstaubach@gmail.com>
2023-06-01 15:18:29 -0400

    added associations for course

--- practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
@@ -10,4 +10,17 @@
 #  updated_at    :datetime         not null
 #
 class Course < ApplicationRecord
+    has_many :enrollments,
+        primary_key: :id,
+        foreign_key: :enrollments_id,
+        class_name: :Enrollment
+    
+    has_many :enrolled_students,
+        through: :enrollments,
+        source: :student
+
+    belongs_to :prereq,
+        primary_key: :id,
+        foreign_key: :prereq_id,
+        class_name: :Course
 end
--- practice-for-ch-sql-associations-long-practice-main/app/models/user.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/user.rb
@@ -8,4 +8,13 @@
 #  updated_at :datetime         not null
 #
 class User < ApplicationRecord
+    has_many :enrollments,
+        primary_key: :id,
+        foreign_key: :enrollments_id,
+        class_name: :Enrollment
+    
+    has_many :enrolled_courses,
+        through: :enrollments,
+        source: :course
+    
 end
